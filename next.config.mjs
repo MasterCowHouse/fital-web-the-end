fcc6b39e6c6d7f577751ace901942c09203d35a3
leonardomoralesgustavo <leonardomoralesgustavo@gmail.com>
2025-01-07 10:55:15 -0600

    test deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+},
   reactStrictMode: false,
   env: {
     CRM_URL: process.env.CRM_URL,
