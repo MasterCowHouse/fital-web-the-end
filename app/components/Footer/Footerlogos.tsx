bc737ee425d8e69e1197ed31297d13afbf64a8ab
Ivan Santana <ivansantana@Ivans-Mac-mini.local>
2025-01-13 10:41:09 -0600

    Add terms and privacy text

--- app/components/Footer/Footerlogos.tsx
+++ app/components/Footer/Footerlogos.tsx
@@ -1,6 +1,8 @@
-import { Box, Typography } from "@mui/material";
-import Link from "next/link";
-
+"use client";
+import { Box, Button, Fade, Modal, Typography } from "@mui/material";
+import Link from "@mui/material/Link";
+import { useState } from "react";
+import Backdrop from "@mui/material/Backdrop";
 interface CustomLogosFooter {
   id: number;
   logosFooter: string;
@@ -38,6 +40,9 @@ const customLogosFooter: CustomLogosFooter[] = [
 ];
 
 const Footerlogos = () => {
+  const [openTerms, setOpenTerms] = useState<boolean | null>(null);
+  const [openPrivacy, setOpenPrivacy] = useState<boolean | null>(null);
+
   return (
     <Box
       sx={{
@@ -59,8 +64,249 @@ const Footerlogos = () => {
           flexDirection: { xs: "column", sm: "row", md: "row", lg: "row" },
         }}
       >
-        <Typography>Términos y condiciones</Typography>
-        <Typography>Política de privacidad</Typography>
+        <Link component={"button"} onClick={() => setOpenTerms(true)}>
+          <Typography>Términos y condiciones</Typography>
+        </Link>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={openTerms!}
+          onClose={() => setOpenTerms(false)}
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+          slotProps={{
+            backdrop: {
+              timeout: 500,
+            },
+          }}
+        >
+          <Fade in={openTerms!}>
+            <Box sx={style}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flex: 1,
+                  flexDirection: "column",
+                  color: "#999696",
+                  p: 4,
+                  marginBottom: 10,
+                }}
+              >
+                <Typography
+                  id="transition-modal-title"
+                  variant="h6"
+                  component="h2"
+                  sx={{
+                    color: "#FFFFFF",
+                    textAlign: "justify",
+                    mb: 3,
+                  }}
+                >
+                  ACEPTACION DE TERMINOS <br />
+                  Acepta y otorga los permisos adecuados de la utilización y
+                  acceso a nuestra página web donde FITAL le hace saber a usted
+                  que expresamente acepta dicha autorización para que la
+                  información que se recabe al ingresar a nuestro portal, siendo
+                  así de vital importancia la atención a sus datos sin que estos
+                  sean vulnerados brindara un manejo adecuado de los mismos,
+                  proporcionando seguridad y protección cumpliendo la finalidad
+                  para la cual fueron recabados, sin promover la discriminación,
+                  racismo, o alguna otra distinción malintencionada que ponga en
+                  peligro la integridad de los involucrados, haciendo valer sus
+                  derechos únicos e inviolables de cada uno.
+                  <br />
+                  FITAL MX se reserva el derecho de actualizar la presente
+                  Política de privacidad en cualquier momento mediante la
+                  publicación de una versión revisada en LOS SITIOS. La versión
+                  revisada entrará en vigor en el momento de su publicación.
+                  Además, si la versión revisada incluye un cambio sustancial,
+                  se lo notificaremos con razonable anticipación publicando una
+                  notificación en LOS SITIOS. Después de este período de
+                  notificación, se considerará que ha aceptado expresamente
+                  todas las modificaciones a esta política. <br />
+                  En caso de que ocurra una vulneración de seguridad en
+                  cualquier fase del tratamiento de datos personales, FITAL MX
+                  lo hará del conocimiento general, de manera inmediata en
+                  nuestro sitio web o mediante notificación personal para que
+                  las personas involucradas en la vulneración de sus datos
+                  personales tomen las medidas correspondientes a fin de
+                  resguardar sus derechos. <br />
+                  Este aviso se apega a los lineamientos emitidos por el
+                  Instituto Federal de Acceso a la Información y Protección de
+                  Datos Personales (IFAI) así como a los preceptos de la Ley
+                  Federal de Protección de Datos Personales en Posesión de los
+                  Particulares.
+                  <br />
+                  Todos los derechos reservados, FITAL MX.
+                </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    borderColor: "#FFC72C",
+                    border: 2,
+                    borderRadius: 20,
+                    width: 170,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    alignSelf: "center",
+                  }}
+                >
+                  <Button
+                    sx={{
+                      color: "#FFFFFF",
+                      display: "flex",
+                      width: "100%",
+                      borderRadius: 20,
+                    }}
+                    onClick={() => setOpenTerms(false)}
+                  >
+                    OK
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          </Fade>
+        </Modal>
+        <Link component={"button"} onClick={() => setOpenPrivacy(true)}>
+          <Typography>Política de privacidad</Typography>{" "}
+        </Link>
+
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={openPrivacy!}
+          onClose={() => setOpenPrivacy(false)}
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+          slotProps={{
+            backdrop: {
+              timeout: 500,
+            },
+          }}
+        >
+          <Fade in={openPrivacy!}>
+            <Box sx={style}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flex: 1,
+                  flexDirection: "column",
+                  color: "#999696",
+                  p: 4,
+                  marginBottom: 10,
+                }}
+              >
+                <Typography
+                  id="transition-modal-title"
+                  variant="h6"
+                  component="h2"
+                  sx={{
+                    color: "#FFFFFF",
+                    textAlign: "justify",
+                    mb: 3,
+                  }}
+                >
+                  Reconoce que nos han proporcionado los datos de contacto, como
+                  datos personales, nuestro compromiso es óptimo para tratar
+                  estos datos para los fines de promoción o publicidad, para
+                  propios y terceros que pueden ser de interés, en nuestra
+                  plataforma o sitio web tanto como ofrecer promociones
+                  comerciales de bienes o servicios o cualquier otra
+                  información, manifestando su consentimiento al aceptar el
+                  mismo.
+                  <br />
+                  <br />
+                  DATOS PERSONALES SENSIBLES. <br />
+                  Nuestro aviso de privacidad FITAL MX denominado por el
+                  (Responsable) será encargado de llevar los ciertos datos
+                  personales que serán proporcionados a través de usted
+                  denominado el (Titular), para efectos del presente aviso
+                  señala. Datos Personales que están sometidos a resguardo. Los
+                  datos proporcionados por parte del el Titular y revisión de
+                  los mismos en el sitio web son: Datos de identificación
+                  (Nombre (s), Apellido (s), Correo Electrónico y Número
+                  Telefónico, RFC). El Titular no debe deberá añadir, otorgar
+                  otros datos no requeridos o diferentes a los ya solicitados
+                  por el Responsable. FITAL MX informa que los datos personales
+                  quedan bajo resguardo y se compromete a manejarlos con
+                  confidencialidad, seguridad, mientras tanto en ningún momento
+                  se solicitaran datos personales sensibles o de carácter intimo
+                  condenando la utilización indebida que pueda dar origen a
+                  discriminación por su origen racial, étnico, creencia
+                  religiosa, preferencia sexual entre otras.
+                  <br /> <br />
+                  USO DE LOS DATOS REMOTOS. <br /> Al visitar nuestro sitio web,
+                  así como los sitios, aplicaciones y herramientas relacionadas
+                  con los servicios, FITAL MX podrá recabar información que nos
+                  haya sido enviada por su dispositivo de acceso, ya sea
+                  computadora, teléfono móvil u otros conocidos o por conocerse,
+                  entre la que se encuentra: datos sobre las páginas a las que
+                  obtiene acceso, la dirección IP y/o identificadores de su
+                  dispositivo, sistema operativo en uso, ubicación, la
+                  información de la red móvil, los datos de registro web
+                  estándar, navegador que está utilizando y el tráfico hacia y
+                  desde nuestro sitio e información adicional, como su uso e
+                  interacción con nuestro sitio web en relación con los
+                  servicios. Cuando hace uso de los servicios, FITAL MX recopila
+                  información acerca de las interacciones que consulta a través
+                  del sitio web, En caso de requerimiento, puede ponerlas a
+                  disposición de las autoridades competentes en México y de
+                  aquellas con competencia en virtud de los Tratados
+                  Internacionales de los que México sea parte. Así mismo, al
+                  acceder a nuestro sitio web o utilizar los servicios, FITAL MX
+                  podrá colocar archivos de datos codificados (cookies,
+                  etiquetas de píxel, {"'"}Flash cookies
+                  {"'"} u otros medios de almacenamiento local proporcionados
+                  por el navegador o las aplicaciones asociadas, denominados
+                  {"'"}cookies{"'"}). El uso de estas tecnologías por FITAL MX
+                  tiene como finalidad su reconocimiento y personalización como
+                  usuario de los servicios, haciendo más segura su cuenta y más
+                  eficaces los mismos. Usted puede optar por bloquear, eliminar
+                  o deshabilitar las “cookies” directamente en su navegador.{" "}
+                  <br /> <br />
+                  EJERCICIO DE LOS DERECHOS DEL ARCO <br />
+                  Con apego a lo previsto por la Ley, FITAL MX cuenta con
+                  procedimientos sencillos y eficaces para atender a las
+                  solicitudes de derechos de Acceso, Rectificación, Cancelación
+                  y Oposición que llegase a presentar, los cuales podrán ser
+                  ejercidos mediante formulario que podrá obtenerse mediante
+                  previa solicitud mediante correo electrónico. El correo
+                  electrónico para recibir sus dudas o manifestaciones respecto
+                  de sus derechos ARCO es el siguiente: contacto@fitalmx.com, en
+                  donde en caso de solicitarlo le haremos conocer el
+                  procedimiento para ello.
+                  <br />
+                  <br />
+                  Todos los derechos reservados, FITAL MX.
+                </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    borderColor: "#FFC72C",
+                    border: 2,
+                    borderRadius: 20,
+                    width: 170,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    alignSelf: "center",
+                  }}
+                >
+                  <Button
+                    sx={{
+                      color: "#FFFFFF",
+                      display: "flex",
+                      width: "100%",
+                      borderRadius: 20,
+                    }}
+                    onClick={() => setOpenPrivacy(false)}
+                  >
+                    OK
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          </Fade>
+        </Modal>
         <Typography>Preguntas frecuentes</Typography>
       </Box>
       <Box
@@ -88,13 +334,26 @@ const Footerlogos = () => {
           padding: "0px 0px 100px 0px",
         }}
       >
-        <Typography>
-          {/* &copy; or &#169; codigo para el copyright */}
-          2024 &copy; Todos los derechos reservados
-        </Typography>
+        <Typography>2024 &copy; Todos los derechos reservados</Typography>
       </Box>
     </Box>
   );
 };
 
 export default Footerlogos;
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  overflowY: "auto",
+  width: { xs: "75vw", sm: "65vw", md: "35vw" },
+  height: { xs: "60vh", sm: "62vh", md: "60vh", lg: "50vh" },
+  bgcolor: "#210044",
+  boxShadow: 30,
+  p: 1,
+  borderRadius: "40px",
+  flex: 1,
+  display: "flex",
+};
