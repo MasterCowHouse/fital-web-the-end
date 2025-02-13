f02b02a648d35afa06b41be54ecb1001131679e5
leonardomoralesgustavo <leonardomoralesgustavo@gmail.com>
2025-02-13 10:42:21 -0600

    bugs de Ximena

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -41,6 +41,7 @@ export default async function RootLayout({
     <NextIntlClientProvider messages={messages}>
       <html lang={locale}>
         <head>
+          {/***********  inicio de todo tag manager ************/}
           {/* Google Tag Manager */}
           <script>
             {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -58,15 +59,10 @@ export default async function RootLayout({
               width="0"
             ></iframe>
           </noscript>
-          {/* Google Tag Manager */}
-          <script>
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KMWMNCDC');`}
-          </script>
-          {/* End Google Tag Manager */}
+          {/* End Google Tag Manager noscript */}
+
+          {/***********  fin de todo tag manager ************/}
+
           {/* Google tag */}
           <script
             async
@@ -88,35 +84,12 @@ export default async function RootLayout({
           ></script>
           {/* End Cookiebot */}
         </head>
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-RJ92NGTRWX"
-          strategy="afterInteractive" // Carga después de que la página esté interactiva
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-RJ92NGTRWX');
-          `}
-        </Script>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           style={{
             backgroundImage: `linear-gradient(222deg, rgba(82,64,146,1) 0%, rgba(57,47,97,1) 19%, rgba(48,41,80,1) 44%, rgba(47,40,78,1) 62%, rgba(25,21,41,1) 83%, rgba(13,11,21,1) 100%)`,
           }}
         >
-          {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-KMWMNCDC"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
-          </noscript>
-          {/* End Google Tag Manager (noscript) */}
           <TopNav />
           {children}
           <Footer />
